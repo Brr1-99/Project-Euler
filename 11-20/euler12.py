283f2bc94187e9381499879b166e982b46d311fe
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2022-06-16 18:25:07 +0200

    folder creation

---
+++ 11-20/euler12.py
@@ -0,0 +1,30 @@
+"""
+What is the value of the first triangle number to have over five hundred divisors?
+"""
+
+import math
+
+def getDivisors(n):
+    divisors = [1]
+    i = 1
+    while i<= math.sqrt(n):
+        if n % i == 0:
+            if n / i == i:
+                divisors.append(i)
+            else:
+                divisors.append(i)
+                divisors.append(int(n/i))
+        i = i + 1
+    return divisors
+
+num_divisors = [1]
+
+i = 1
+
+while len(num_divisors) <= 500:
+    new_div = getDivisors(sum(range(i+1)))
+    if new_div > num_divisors:
+        num_divisors = new_div
+    i+= 1
+
+print(f'The first triangle number with over 500 divisors is {sum(range(i))} with a total of {len(num_divisors)}')
--- euler11.py
+++
@@ -1,3 +0,0 @@
-"""
-What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20*20 grid?
-"""
