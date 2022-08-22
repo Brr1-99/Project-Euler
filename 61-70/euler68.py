bb3f0d9cc274d697ea39dbc8be0104caf82c847d
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2022-08-22 20:14:36 +0200

    Create euler68.py

---
+++ 61-70/euler68.py
@@ -0,0 +1,11 @@
+"""
+Using the numbers 1 to 10, and depending on arrangements, it is possible to form 16- and 17-digit strings. 
+
+What is the maximum 16-digit string for a "magic" 5-gon ring?
+"""
+
+"""
+Solved by paper and pen
+"""
+
+print('The maximum 16-digit string for a "magic" 5-gon ring is: 6531031914842725 ')
